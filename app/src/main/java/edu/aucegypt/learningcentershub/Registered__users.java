348b32cc752df53a8c1d8fe5b3424cbb018182bd
Goldent00thbrush <44409654+Goldent00thbrush@users.noreply.github.com>
2020-01-20 13:45:28 +0200

    recycler views

--- app/src/main/java/edu/aucegypt/learningcentershub/Registered__users.java
+++ app/src/main/java/edu/aucegypt/learningcentershub/Registered__users.java
@@ -1,17 +1,25 @@
 package edu.aucegypt.learningcentershub;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import androidx.fragment.app.Fragment;
+import android.widget.TextView;
 
-public class Registered__users extends Fragment implements AdapterView.OnItemClickListener {
-    ListView listView;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Registered__users extends Fragment  {
+    RecyclerView listView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -19,19 +27,63 @@ public class Registered__users extends Fragment implements AdapterView.OnItemCli
         View view =  inflater.inflate(R.layout.registered_users, container, false);
         listView = view.findViewById(R.id.registred_users_users);
         String[] Data = {"User 1", "User 2"}; //comes from database
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, android.R.id.text1, Data);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(this);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        listView.setLayoutManager(layoutManager);
+        listView.setAdapter(new rvadapter3(getContext(), Data));
+        listView.setNestedScrollingEnabled(false);
         return view;
     }
 
 
+}
+
+
+class rvadapter3 extends RecyclerView.Adapter<rvadapter3.ViewHolder3> implements View.OnClickListener{
+    String[] rows;
+    public Context mContext;
+
+    public rvadapter3(Context context, String[] Names) {
+        this.rows = Names;
+        this.mContext = context;
+    }
+    @NonNull
+    @Override
+    public ViewHolder3 onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        view.setOnClickListener(this);
+        return  new ViewHolder3(view);
+
+    }
 
     @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-        Fragment selectedFragment = null;
-                selectedFragment = new user_info_frag();
+    public void onBindViewHolder(@NonNull ViewHolder3 holder, int position) {
+        holder.text1.setText(rows[position]);
 
-        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment2_2, selectedFragment).commit();
+    }
+
+    @Override
+    public int getItemCount() {
+        return rows.length;
+    }
+    @Override
+    public void onClick(View view) {
+
+        Fragment selectedFragment = null;
+        selectedFragment = new user_info_frag();
+
+        ((FragmentActivity)mContext).getSupportFragmentManager().beginTransaction().replace(R.id.fragment2_2, selectedFragment).commit();
+    }
+    public class ViewHolder3 extends RecyclerView.ViewHolder  {
+
+        TextView text1;
+
+        public ViewHolder3(View itemView) {
+            super(itemView);
+            text1 = itemView.findViewById(android.R.id.text1);
+        }
+
+
     }
 }
+
+
