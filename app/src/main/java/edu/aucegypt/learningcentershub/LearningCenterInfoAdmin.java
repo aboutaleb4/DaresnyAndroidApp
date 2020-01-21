c73a470765d00e17b7cc40b39a8114ebfd081f8b
Goldent00thbrush <44409654+Goldent00thbrush@users.noreply.github.com>
2020-01-21 19:18:28 +0200

    in progress

---
+++ app/src/main/java/edu/aucegypt/learningcentershub/LearningCenterInfoAdmin.java
@@ -0,0 +1,15 @@
+package edu.aucegypt.learningcentershub;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LearningCenterInfoAdmin extends AppCompatActivity {
+
+    @Override
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learningcenter_info_admin);
+
+    }
+}
